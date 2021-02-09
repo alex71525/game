586ed944088dce979f4dc623fa42617ea47689c4
alex71525 <76918410+alex71525@users.noreply.github.com>
2021-02-09 21:00:07 +0400

    Update main.py

--- main.py
+++ main.py
@@ -69,6 +69,7 @@ def playerHasHitShield(playerRect, shields):
     global shieldCount, shieldUse, t, k
     for i in shields:
         if playerRect.colliderect(i['rect']):
+            shieldSound.play()
             shields.remove(i)
             shieldCount = 0
             win.blit(shieldEffect, shieldRect)
@@ -81,9 +82,26 @@ def meteorHasHitShield(playerRect, meteors):
     global shieldUse
     for b in meteors:
         if playerRect.colliderect(b['rect']):
+            shieldBreak.play()
             meteors.remove(b)
             shieldUse = False
 
+
+def playerHasHitBonus(playerRect, bullets):
+    global bulletsQuantity
+    for i in bullets:
+        if playerRect.colliderect(i['rect']):
+            gunReload.play()
+            bullets.remove(i)
+            bulletsQuantity += 10
+
+
+def meteorHasHitBullet(meteors, bulletsEffect):
+    for i in meteors:
+        for j in bulletsEffect:
+            if j['rect'].colliderect(i['rect']):
+                bulletsEffect.remove(j)
+                meteors.remove(i)
 
 def drawText(text, font, surface, x, y):
     textobj = font.render(text, 1, textColor)
@@ -104,10 +122,16 @@ font1 = pygame.font.SysFont(None, 32)
 gameOverSound = pygame.mixer.Sound('game_over.wav')
 collisionSound = pygame.mixer.Sound('collisionSound.wav')
 healSound = pygame.mixer.Sound('healEffect.wav')
+gunSound = pygame.mixer.Sound('gunSound.wav')
+gunReload = pygame.mixer.Sound('gunReload.mp3')
+shieldSound = pygame.mixer.Sound('shieldSound.mp3')
+shieldBreak = pygame.mixer.Sound('shieldBreak.wav')
 pygame.mixer.music.load('backgroundMusic.wav')
 gameOverSound.set_volume(0.5)
 healSound.set_volume(0.3)
 collisionSound.set_volume(0.35)
+gunSound.set_volume(0.25)
+shieldBreak.set_volume(0.8)
 
 playerImage = pygame.image.load('ship.png')
 playerRect = playerImage.get_rect()
@@ -118,6 +142,9 @@ emptyHeartImage = pygame.image.load('heartEmpty.png')
 shield = pygame.image.load('shield.png')
 shieldEffect = pygame.image.load('shieldEffect.png')
 shieldRect = shieldEffect.get_rect()
+bullet = pygame.image.load('bullet.png')
+bulletRect = bullet.get_rect()
+bonus = pygame.image.load('bonus.png')
 zaplatka = pygame.image.load('background1.png')
 
 win.blit(backgroundImage, (0, 0))
@@ -131,18 +158,23 @@ topScore = 0
 while True:
     meteorMinSpeed = 1
     meteorMaxSpeed = 5
+    bulletSpeed = -20
     addNewMeteorRate = 14
     meteors = []
     hearts = []
     shields = []
     emptyHearts = []
+    bullets = []
+    bulletsEffect = []
     heartChange = False
     shieldUse = False
+    shoot = False
     shieldCount = 0
     extraLife = []
     score = 0
     moreSpeedCount = 0
     heartQuantity = 3
+    bulletsQuantity = 10
     topHeartQuantity = heartQuantity
     playerRect.topleft = (winWidth / 2, winHeight - 85)
     moveLeft = moveRight = moveUp = moveDown = False
@@ -184,6 +216,10 @@ while True:
                 if event.key == K_DOWN or event.key == K_s:
                     moveDown = False
 
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    shoot = True
+
             if event.type == MOUSEMOTION:
                 if event.pos[0] < winWidth - 64:
                     playerRect.left = event.pos[0]
@@ -211,9 +247,15 @@ while True:
 
         randomNumber1 = random.randint(0, 3500)
         if randomNumber1 == 7 and score > 1000:
-            newShield = {'rect': pygame.Rect(random.randint(0, winWidth - 40), -34, 40, 34),
+            newShield = {'rect': pygame.Rect(random.randint(0, winWidth - 32), -32, 32, 32),
                          'speed': random.randint(2, 5)}
             shields.append(newShield)
+
+        randomNumber2 = random.randint(0, 2000)
+        if randomNumber2 == 7:
+            newBonus = {'rect': pygame.Rect(random.randint(0, winWidth - 22), -29, 22, 29),
+                        'speed': random.randint(2, 5)}
+            bullets.append(newBonus)
 
         if score % 2 == 0:
             for b in meteors:
@@ -222,6 +264,10 @@ while True:
                 i['rect'].move_ip(0, i['speed'])
             for i in shields:
                 i['rect'].move_ip(0, i['speed'])
+            for i in bullets:
+                i['rect'].move_ip(0, i['speed'])
+            for i in bulletsEffect:
+                i['rect'].move_ip(0, bulletSpeed)
 
         for b in meteors[:]:
             if b['rect'].top > winHeight:
@@ -232,6 +278,9 @@ while True:
         for i in extraLife[:]:
             if i['rect'].top > winHeight:
                 extraLife.remove(i)
+        for i in bullets[:]:
+            if i['rect'].bottom < 0:
+                bullets.remove(i)
 
         if moreSpeedRate == moreSpeedCount:
             meteorMinSpeed += 1
@@ -263,6 +312,10 @@ while True:
             win.blit(fullHeartImage, i['rect'])
         for i in shields:
             win.blit(shield, i['rect'])
+        for i in bullets:
+            win.blit(bonus, i['rect'])
+        for i in bulletsEffect:
+            win.blit(bullet, i['rect'])
         for i in hearts:
             win.blit(fullHeartImage, i['rect'])
         for i in emptyHearts:
@@ -299,6 +352,15 @@ while True:
                 t = 60
                 k = 0
 
+        playerHasHitBonus(playerRect, bullets)
+        if shoot and bulletsQuantity > 0:
+            gunSound.play()
+            bulletsQuantity -= 1
+            newBullet = {'rect': pygame.Rect(playerRect.centerx - 5, playerRect.centery, 11, 16)}
+            bulletsEffect.append(newBullet)
+
+        meteorHasHitBullet(meteors, bulletsEffect)
+
         playerHasHitHeart(playerRect, extraLife)
         if heartQuantity > topHeartQuantity:
             topHeartQuantity += 1
@@ -322,9 +384,11 @@ while True:
                     emptyHearts.append(newEmptyHeart)
 
         heartChange = False
+        shoot = False
 
         drawText('Score: %s' % {score}, font1, win, 5, 0)
         drawText('Record %s' % {topScore}, font1, win, 5, 30)
+        drawText('Bullets: %s' % bulletsQuantity, font1, win, 5, 100)
 
         pygame.display.flip()
         mainClock.tick(FPS)
